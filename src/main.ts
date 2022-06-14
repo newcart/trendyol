bb92942a47a0cdea25c636607e7123d5ef3abe45
tulparstudyo <teknovipyazilim@gmail.com>
2022-06-14 20:25:17 +0300

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -6,9 +6,9 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.connectMicroservice({
     transport: Transport.TCP,
-    options: { host: '0.0.0.0', port: 30101 },
+    options: { host:'0.0.0.0', port: 30101 },
   });
   await app.startAllMicroservices();
-  console.log(`Trendyol Service is listening on 30101`);
+  console.log('Trendyol Service is runing on 30101.');
 }
 bootstrap();
