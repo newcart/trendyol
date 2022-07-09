aa44460cd12c9e22a724a1805075d5f0a0dd502a
tulparstudyo <teknovipyazilim@gmail.com>
2022-07-10 00:46:25 +0300

    env işleri

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,6 @@ import { Transport } from '@nestjs/microservices';
 import { AppModule } from './app.module';
 import { ConfigService } from "@nestjs/config";
 
-
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const configService = app.get(ConfigService);
@@ -13,6 +12,11 @@ async function bootstrap() {
     options: { host:configService.get('HOST')||'0.0.0.0', port: configService.get('PORT')||30101 },
   });
   await app.startAllMicroservices();
-  console.log('Trendyol Service is runing on ' + (configService.get('HOST')||'0.0.0.0') + ':' + (configService.get('PORT')||30101 ));
+  console.log(
+      (configService.get('SERVICE_NAME')||'UNNAMED') +
+      ' Service is runing on ' +
+      (configService.get('HOST')||'0.0.0.0') +
+      ':' +
+      (configService.get('PORT')||30101 ));
 }
 bootstrap();
