8762ce432a21e47bdd0c4974ca05f4bbc88a0c1a
tulparstudyo <teknovipyazilim@gmail.com>
2022-07-10 12:01:10 +0300

    Env işlemleri

--- src/main.ts
+++ src/main.ts
@@ -6,17 +6,14 @@ import { ConfigService } from "@nestjs/config";
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const configService = app.get(ConfigService);
-
+  const HOST = configService.get('HOST')||'0.0.0.0'
+  const PORT = configService.get('PORT')||30101
+  const SERVICE_NAME = configService.get('SERVICE_NAME')||'TRENDYOL'
   app.connectMicroservice({
     transport: Transport.TCP,
-    options: { host:configService.get('HOST')||'0.0.0.0', port: configService.get('PORT')||30101 },
+    options: { host:HOST, port: PORT },
   });
   await app.startAllMicroservices();
-  console.log(
-      (configService.get('SERVICE_NAME')||'UNNAMED') +
-      ' Service is runing on ' +
-      (configService.get('HOST')||'0.0.0.0') +
-      ':' +
-      (configService.get('PORT')||30101 ));
+  console.log( SERVICE_NAME + ' Service is runing on ' + HOST + ':' + PORT);
 }
 bootstrap();
